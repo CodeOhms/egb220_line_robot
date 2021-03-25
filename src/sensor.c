b790e19622aaa6d9006a7979b3d74a1cc3cd8045
kevin <71308292+broke-ch@users.noreply.github.com>
2021-03-25 23:25:11 +1000

    modified:   sensor.c

--- src/sensor.c
+++ src/sensor.c
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 
 void adc_init(){
-	ADMUX |= (1<<REFS0)|(1<<ADLAR); // 5V ref, left adjusted
+	ADMUX |= (1<<REDS1)|(1<<REFS0)|(1<<ADLAR); // 2.56V ref, left adjusted
 	// enable adc, enable auto-trigger, enable interrupt & interrupt flag, clock prescaler of 128, start conversion
 	ADCSRA |= (1<<ADEN)|(1<<ADATE)|(1<<ADIE)|(1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0);
 	ADCSRB = 0;
@@ -24,13 +24,12 @@ void sen_5() {
 	else{
 		PORTE &= ~(1<<6);
 	}
-		ADCSRA |= (1<<4);
+		ADCSRA |= (1<<ADIF); // Clear AD Flag BIT (leave at 1)
 }
 
 void sen_4() {
-	ADMUX |= (<<MUX2); // enable sensor 4
+	ADMUX |= (1<<MUX2); // enable sensor 4
 	ADCSRA |= (1<<6); // start converstion
-
 	while(!(ADCSRA&(1<<ADIF))){}
 	// Result now available.
 	uint16_t pot2 = ADCH;
@@ -40,10 +39,11 @@ void sen_4() {
 	else{
 		PORTB &= ~(1<<2);
 	}
-		ADCSRA |= (1<<4);
+		ADCSRA |= (1<<ADIF); // CLEAR FLAG
 }
 
 int main(){
+	adc_init();
 	DDRB |= (1<<2); // set LED 0
 	DDRE |= (1<<6); // set LED 3
 	
