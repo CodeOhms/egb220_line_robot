c6a2f586102a19931f7b0f092f2eb51f16b4a687
kevin <71308292+broke-ch@users.noreply.github.com>
2021-03-25 23:04:04 +1000

    modified:   sensor.c

--- src/sensor.c
+++ src/sensor.c
@@ -4,45 +4,47 @@
 #include <avr/interrupt.h>
 #include <stdlib.h>
 
-void sen_5() {
-	ADMUX |= (1<<6)|(1<<5)|(1<<1)|(1<<0); //enable sensor 5, 5V ref, left adjusted
+void adc_init(){
+	ADMUX |= (1<<REFS0)|(1<<ADLAR); // 5V ref, left adjusted
 	// enable adc, enable auto-trigger, enable interrupt & interrupt flag, clock prescaler of 128, start conversion
-	ADCSRA |= (1<<7)|(1<<5)|(1<<3)|(1<<2)|(1<<1)|1;
-	ADCSRB |= (1<<5); // free running mode, mux 5 on
-	ADCSRA |= (1<<6); // start conversion
+	ADCSRA |= (1<<ADEN)|(1<<ADATE)|(1<<ADIE)|(1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0);
+	ADCSRB = 0;
+}
 
-	while(~ADCSRA&(1<<ADIF)){} // Result now available.
+void sen_5() {
+	ADMUX |= (1<<MUX1)|(1<<MUX0); //enable sensor 5
+	ADCSRB |= (1<<MUX5); // free running mode, mux 5 on
+	ADCSRA |= (1<<ADSC); // start conversion
+
+	while(!(ADCSRA&(1<<ADIF))){} // Result now available.
 	uint16_t pot3 = ADCH;
-	if (pot3 < 50) {
+	if (pot3 > 50) {
 		PORTE = (1<<6);
 	}
 	else{
 		PORTE &= ~(1<<6);
 	}
-		ADCSRA |= (0<<ADIF);
+		ADCSRA |= (1<<4);
 }
+
 void sen_4() {
-   // Start single conversion by setting ADSC bit in ADCSRA
-	ADMUX |= (1<<6)|(1<<5)|(1<<2); // enable sesor 4, 5V ref. left adjusted.
-	// enable adc, enable auto-trigger, enable interrupt & interrupt flag, clock prescaler of 128, start conversion
-	ADCSRA |= (1<<7)|(1<<5)|(1<<3)|(1<<2)|(1<<1)|1;
-	ADCSRB = 0; // free running mode
+	ADMUX |= (<<MUX2); // enable sensor 4
 	ADCSRA |= (1<<6); // start converstion
 
-	while(~ADCSRA&(1<<ADIF)){}
+	while(!(ADCSRA&(1<<ADIF))){}
 	// Result now available.
 	uint16_t pot2 = ADCH;
-	if (pot2 < 50){
+	if (pot2 > 50){
 		PORTB = (1<<2);
-		ADCSRA |= (0<<ADIF);
 	}
 	else{
 		PORTB &= ~(1<<2);
 	}
-		ADCSRA |= (0<<ADIF);
+		ADCSRA |= (1<<4);
 }
+
 int main(){
-	DDRB |= (1<<0); // set LED 0
+	DDRB |= (1<<2); // set LED 0
 	DDRE |= (1<<6); // set LED 3
 	
 	while(1) {
