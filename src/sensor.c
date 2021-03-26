437b5fb6477b75f08aac3863d51d421e7dce8e52
kevin <71308292+broke-ch@users.noreply.github.com>
2021-03-26 17:32:11 +1000

    added straight line following code

--- src/sensor.c
+++ src/sensor.c
@@ -4,8 +4,26 @@
 #include <avr/interrupt.h>
 #include <stdlib.h>
 
-// void adc_init()
-// 
+uint16_t pot3;
+uint16_t pot2;
+
+void pwm_init(){
+	TCCR0A |= (1<<WGM00);
+	TCCR0A |= (1<<WGM01);
+	TCCR0A &= ~(1<<WGM02);
+
+	TCCR0B |= (1<<CS02);
+	TCCR0B &= ~(1<<CS01);
+	TCCR0B &= ~(1<<CS00);
+
+	TCCR0A |= (1<<COM0B1);
+	TCCR0A &= ~(1<<COM0B0);
+
+	TCCR0A |= (1<<COM0A1);
+	TCCR0A &= ~(1<<COM0A0);
+	// motor A uses OCR0A
+	// motor A uses OCR0B
+}
 
 ISR(ADC_vect0)
 {
@@ -15,20 +33,20 @@ ISR(ADC_vect0)
 void sen_5() {
 	ADMUX |= (1<<6)|(1<<5)|(1<<1)|(1<<0); //enable sensor 5, 5V ref, left adjusted
 	// enable adc, enable auto-trigger, enable interrupt & interrupt flag, clock prescaler of 128, start conversion
-	ADCSRA |= (1<<7)|(1<<5)|(1<<4)|(1<<3)|(1<<2)|(1<<1)|1;
+	ADCSRA |= (1<<7)|(1<<5)|(1<<4)|(1<<3)(1<<2)|(1<<1)|1;
 	ADCSRB |= (1<<5); // free running mode, mux 5 on
 	ADCSRA |= (1<<6); // start conversion
 
 	while(~ADCSRA&(1<<ADIF)){} // Result now available.
-	uint16_t pot2 = ADCH;
-	if (pot2 < 50) {
-		PORTE = (1<<6);
-	}
-	else{
-		PORTE &= ~(1<<6);
-	}
+	pot2 = ADCH;
+	//if (pot2 < 30) {
+	//	PORTE = (1<<6);
+	//}
+	//else{
+	//	PORTE &= ~(1<<6);
+	//}
 		ADCSRA |= (1<<ADIF); //clear adif
-		ADCSRA &= ~(1<<6); //stop conversion
+		ADCSRA &= ~(1<<6); // stop conversion
 		ADMUX &= ~(1<<1)|(1<<0); // clear admux channels
 		ADCSRB &= ~(1<<5); // clear admux channels
 }
@@ -43,25 +61,64 @@ void sen_4() {
 
 	while(~ADCSRA&(1<<ADIF)){}
 	// Result now available.
-	uint16_t pot3 = ADCH;
-	if (pot3 < 50){
-		PORTB = (1<<2);
-		ADCSRA |= (0<<ADIF);
-	}
-	else{
-		PORTB &= ~(1<<2);
-	}
+	  pot3 = ADCH;
+	//if (pot3 < 30){
+	//	PORTB = (1<<2);
+	//	ADCSRA |= (0<<ADIF);
+	//}
+	//else{
+	//	PORTB &= ~(1<<2);
+	//}
 		ADCSRA |= (1<<ADIF); // clear ADIF 
 		ADCSRA &= ~(1<<6); // stop conversion
 		ADMUX &= ~(1<<2)|(1<<1)|(1<<1); // clear admux channels
 }
 
 int main(){
-	DDRB |= (1<<2); // set LED 3
-	DDRE |= (1<<6); // set LED 0
+
+	//Motor 2
+	DDRB |= (1<<0); // Set Bit/Pin 0 to 1/Output
+	DDRB |= (1<<7); // Set Bit/Pin 0 to 1/Output
+	//Motor 1
+	DDRE |= (1<<6); // Set Bit/Pin 0 to 1/Output
+	DDRD |= (1<<0); // Set Bit/Pin 0 to 1/Output
+
+	pwm_init();
+
 	while(1) {
-	sen_5();
-  // System logic for driving
-	sen_4();
+		sen_5();
+		sen_4();
+
+		if (pot3 < 237 && pot2 < 237) {
+			PORTB |= (1<<0); // Set Bit/Pin 0 to 1/Output
+			OCR0A = 127; // Set Bit/Pin 0 to 1/Output
+
+			PORTE |= (1<<6); // Set Bit/Pin 0 to 1/Output
+			OCR0B = 127;
+		}		
+		else if(pot3 >= 237 && pot2 < 237){
+			PORTB |= (1<<0); // Set Bit/Pin 0 to 1/Output
+			OCR0A = 130; // Set Bit/Pin 0 to 1/Output
+
+			PORTE |= (1<<6); // Set Bit/Pin 0 to 1/Output
+			OCR0B = 120;
+		}
+		else if(pot3 < 237 && pot2 >= 237){
+			PORTB |= (1<<0); // Set Bit/Pin 0 to 1/Output
+			OCR0A = 120; // Set Bit/Pin 0 to 1/Output
+
+			PORTE |= (1<<6); // Set Bit/Pin 0 to 1/Output
+			OCR0B = 130;
+		}
+		
+		else{
+			PORTB &= ~(1<<0); // Set Bit/Pin 0 to 1/Output
+			//PORTB &= ~(1<<7); // Set Bit/Pin 0 to 1/Output
+			OCR0A = 0; // Set Bit/Pin 0 to 1/Output
+
+			PORTE &= ~(1<<6); // Set Bit/Pin 0 to 1/Output
+			//PORTD &= ~(1<<0); // Set Bit/Pin 0 to 1/Output
+			OCR0B = 0;
+		}
 	}
 }
