290e219a77148f30b67e01a70b6588474fd0c50b
kevin <71308292+broke-ch@users.noreply.github.com>
2021-04-28 11:54:11 +1000

    OPTIMISED DEMO 1B DO NOT TOUCH

--- src/proto/proto.ino
+++ src/proto/proto.ino
@@ -4,8 +4,8 @@
 #include <avr/interrupt.h>
 #include <stdlib.h>
 
-#define BASE 	100
-#define SLIGHT 	95
+#define BASE 	100	
+#define SLIGHT 	97
 #define MID		90 	
 #define TOLERANCE 125
 
@@ -54,30 +54,26 @@ ISR(ADC_vect0)
 
 void adc_init(){
 	ADMUX |= (1<<6)|(1<<5); //5V ref 
-	ADCSRA |= (1<<7)|(1<<5)|(1<<4)|(1<<3)|(1<<2)|(1<<1);
+	ADCSRA |= (1<<7)|(1<<5)|(1<<4)|(1<<3)|(1<<2)|(1<<1)|(1<<0);
 	// enable adc, enable auto-trigger, enable interrupt & interrupt flag, clock prescaler of 128, start converion, left adjusted
 }
 
 void sen_8() {
 	ADCSRB |= (1<<5);	// enable adc 8
-	ADCSRA |= (1<<6);	// start conversion
 
 	while(~ADCSRA&(1<<ADIF)){}	// Result now available.
 		pot8 = ADCH;
 		ADCSRA |= (1<<ADIF);	//clear adif
-		ADCSRA &= ~(1<<6);		// stop conversion
 		ADCSRB &= ~(1<<5);		// clear admux channels
 }
 
 void sen_7() {
 	ADMUX |= (1<<0);	// enable adc 9
 	ADCSRB |= (1<<5);
-	ADCSRA |= (1<<6);	// start conversion
 
 	while(~ADCSRA&(1<<ADIF)){}	// Result now available.
 		pot7 = ADCH;
 		ADCSRA |= (1<<ADIF);	//clear adif
-		ADCSRA &= ~(1<<6);		// stop conversion
 		ADMUX &= ~(1<<0);		// clear admux channels
 		ADCSRB &= ~(1<<5);		// clear admux channels
 }
@@ -85,12 +81,10 @@ void sen_7() {
 void sen_6() {
 	ADMUX |= (1<<1);	//enable adc10
 	ADCSRB |= (1<<5);	// mux 5 on
-	ADCSRA |= (1<<6);	// start conversion
 
 	while(~ADCSRA&(1<<ADIF)){}	// Result now available.
 		pot6 = ADCH;
 		ADCSRA |= (1<<ADIF);	//clear adif
-		ADCSRA &= ~(1<<6);		// stop conversion
 		ADMUX &= ~(1<<1);		// clear admux channels
 		ADCSRB &= ~(1<<5);		// clear admux channels
 }
@@ -98,57 +92,47 @@ void sen_6() {
 void sen_5() {
 	ADMUX |= (1<<1)|(1<<0);	//enable adc11
 	ADCSRB |= (1<<5);		// mux 5 on
-	ADCSRA |= (1<<6);		// start conversion
 
 	while(~ADCSRA&(1<<ADIF)){}		// Result now available.
 		pot5 = ADCH;
 		ADCSRA |= (1<<ADIF);		//clear adif
-		ADCSRA &= ~(1<<6);			// stop conversion
 		ADMUX &= ~((1<<1)|(1<<0));	// clear admux channels
 		ADCSRB &= ~(1<<5);			// clear admux channels
 }
 
 void sen_4() { 
 	ADMUX |= (1<<2)|(1<<1)|(1<<0);			// enable adc7
-	ADCSRA |= (1<<6);						// start conversion
 
 	while(~ADCSRA&(1<<ADIF)){}				// Result now available.
 		pot4 = ADCH;
 		ADCSRA |= (1<<ADIF);				// clear ADIF 
-		ADCSRA &= ~(1<<6);					// stop conversion
 		ADMUX &= ~((1<<2)|(1<<1)|(1<<0));	// clear admux channels
 }
 
 void sen_3() { 
 	ADMUX |= (1<<2)|(1<<1);			// enable adc6
-	ADCSRA |= (1<<6);				// start conversion
 
 	while(~ADCSRA&(1<<ADIF)){}		// Result now available.
 		pot3 = ADCH;
 		ADCSRA |= (1<<ADIF);		// clear ADIF 
-		ADCSRA &= ~(1<<6);			// stop conversion
 		ADMUX &= ~((1<<2)|(1<<1));	// clear admux channels
 }
 
 void sen_2() { 
 	ADMUX |= (1<<2)|(1<<0);			// enable adc5
-	ADCSRA |= (1<<6);				// start conversion
 
 	while(~ADCSRA&(1<<ADIF)){}		// Result now available.
 		pot2 = ADCH;
 		ADCSRA |= (1<<ADIF);		// clear ADIF 
-		ADCSRA &= ~(1<<6);			// stop conversion
 		ADMUX &= ~((1<<2)|(1<<0));	// clear admux channels
 }
 
 void sen_1() { 
 	ADMUX |= (1<<2);				// enable adc4
-	ADCSRA |= (1<<6);				// start conversion
 
 	while(~ADCSRA&(1<<ADIF)){}		// Result now available.
 		pot1 = ADCH;
 		ADCSRA |= (1<<ADIF);		// clear ADIF 
-		ADCSRA &= ~(1<<6);			// stop conversion
 		ADMUX &= ~(1<<2);			// clear admux channels
 }
 
@@ -158,6 +142,7 @@ int main(){
 	pwm_init();
 
 	while(1) {
+		ADCSRA |= (1<<6);				// start conversion
 		sen_8();
 		sen_7();
 		sen_6();
@@ -211,5 +196,6 @@ int main(){
 			OCR0A = 0;
 			OCR0B = 0;
 		}
+		ADCSRA &= ~(1<<6);			// stop conversion
 	}
 }
