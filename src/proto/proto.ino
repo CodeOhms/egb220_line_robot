cbd70013d1e3bf0837f717204258568befe54ce9
kevin <71308292+broke-ch@users.noreply.github.com>
2021-05-06 19:17:25 +1000

    fixed ADC, removed old files

--- src/proto/proto.ino
+++ src/proto/proto.ino
@@ -4,28 +4,30 @@
 #include <avr/interrupt.h>
 #include <stdlib.h>
 
-#define TOLERANCE 80
+#define TOLERANCE 130
 
-//uint16_t pot8;
+uint16_t pot8;
 uint16_t pot7;
 uint16_t pot6;
 uint16_t pot5;
 uint16_t pot4;
 uint16_t pot3;
 uint16_t pot2;
-//uint16_t pot1;
+uint16_t pot1;
 
+int safety = 0;  // TODO if safety = 0, be ready to catch it.
 int error = 0;
 int position = 0;
-int Base = 75;
+int Base = 40;
 int *last_error = 0;
-double Kp = 14;
-double Kd = 0.1;
+int *integral = 0;
+double Kp = 7.5;
+double Kd = 0.08;
 
 void pwm_init(){
 	TCCR0A |= (1<<WGM00);
 	TCCR0A |= (1<<WGM01);
-	TCCR0A &= ~(1<<WGM02);
+	TCCR0B &= ~(1<<WGM02);
 
 	TCCR0B |= (1<<CS02);
 	TCCR0B &= ~(1<<CS01);
@@ -50,86 +52,89 @@ void pwm_init(){
 	PORTE |= (1<<6);
 }
 
-ISR(ADC_vect0)
-{
-	// Only read the sensor values.
-}
 
 void adc_init(){
-	ADMUX |= (1<<6)|(1<<5); //5V ref 
-	ADCSRA |= (1<<7)|(1<<5)|(1<<4)|(1<<3)|(1<<2)|(1<<1);
-	// enable adc, enable auto-trigger, enable interrupt & interrupt flag, clock prescaler of 64, start converion, left adjusted
+	ADMUX |= (1<<REFS1)|(1<<REFS0)|(1<<ADLAR); //2.56V ref, left adjusted
+
+	// enable adc, clock prescaler of 128
+	ADCSRA |= (1<<ADEN)|(1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0);
+	ADCSRB = 0;
 }
 
-//void sen_8() {
-//	ADCSRB |= (1<<5);	// enable adc 8 -> 100000
-//	while(~ADCSRA&(1<<ADIF)){}	// Result now available.
-//		pot8 = ADCH;
-//		ADCSRA |= (1<<ADIF);	//clear adif
-//		ADCSRB &= ~(1<<5);		// clear admux channels
-//}
+void sen_8() {
+	ADMUX = 0b11100000;
+	ADCSRB = 0b00100000;		// enable adc 8 -> 100000
+	ADCSRA |= (1<<6);			// start conversion
+	while(ADCSRA&(1<<ADSC)){} 	// wait for conversion to complete
+	pot8 = ADCH;
+}
 
 void sen_7() {
-	ADMUX |= (1<<0);	// enable adc 9 -> 100001
-	ADCSRB |= (1<<5);
-	while(~ADCSRA&(1<<ADIF)){}	// Result now available.
-		pot7 = ADCH;
-		ADCSRA |= (1<<ADIF);	//clear adif
-		ADMUX &= ~(1<<0);		// clear admux channels
-		ADCSRB &= ~(1<<5);		// clear admux channels
+	ADMUX = 0b11100001; // sensor 7 -> adc9
+	ADCSRB = 0b00100000;
+	ADCSRA |= (1<<6);
+	while(ADCSRA&(1<<ADSC)){}
+	pot7 = ADCH;
 }
 
 void sen_6() {
-	ADMUX |= (1<<1);	//enable adc10 -> 100010
-	ADCSRB |= (1<<5);	// mux 5 on
-	while(~ADCSRA&(1<<ADIF)){}	// Result now available.
-		pot6 = ADCH;
-		ADCSRA |= (1<<ADIF);	//clear adif
-		ADMUX &= ~(1<<1);		// clear admux channels
-		ADCSRB &= ~(1<<5);		// clear admux channels
+	ADMUX = 0b11100010; // sensor 6 -> adc10
+	ADCSRB = 0b00100000;
+	ADCSRA |= (1<<6);
+	while(ADCSRA&(1<<ADSC)){}
+	pot6 = ADCH;
 }
 
 void sen_5() {
-	ADMUX |= (1<<1)|(1<<0);	//enable adc11 -> 100011
-	ADCSRB |= (1<<5);		// mux 5 on
-	while(~ADCSRA&(1<<ADIF)){}		// Result now available.
-		pot5 = ADCH;
-		ADCSRA |= (1<<ADIF);		//clear adif
-		ADMUX &= ~((1<<1)|(1<<0));	// clear admux channels
-		ADCSRB &= ~(1<<5);			// clear admux channels
+	ADMUX = 0b11100011; // sensor 5 -> adc11
+	ADCSRB = 0b00100000;
+	ADCSRA |= (1<<6);
+	while(ADCSRA&(1<<ADSC)){}
+	pot5 = ADCH;
 }
 
 void sen_4() { 
-	ADMUX |= (1<<2)|(1<<1)|(1<<0);			// enable adc7 -> 000111
-	while(~ADCSRA&(1<<ADIF)){}				// Result now available.
-		pot4 = ADCH;
-		ADCSRA |= (1<<ADIF);				// clear ADIF 
-		ADMUX &= ~((1<<2)|(1<<1)|(1<<0));	// clear admux channels
+	ADMUX = 0b11100111; // sensor 4 -> adc7
+	ADCSRB = 0b00000000;
+	ADCSRA |= (1<<6);
+	while(ADCSRA&(1<<ADSC)){}
+	pot4 = ADCH;
 }
 
-void sen_3() { 
-	ADMUX |= (1<<2)|(1<<1);			// enable adc6 -> 000110
-	while(~ADCSRA&(1<<ADIF)){}		// Result now available.
-		pot3 = ADCH;
-		ADCSRA |= (1<<ADIF);		// clear ADIF 
-		ADMUX &= ~((1<<2)|(1<<1));	// clear admux channels
+void sen_3() {  // sensor 3 -> adc6
+	ADMUX = 0b11100110;
+	ADCSRB = 0b00000000;
+	ADCSRA |= (1<<6);
+	while(ADCSRA&(1<<ADSC)){}
+	pot3 = ADCH;
 }
 
-void sen_2() { 
-	ADMUX |= (1<<2)|(1<<0);			// enable adc5 -> 000101
-	while(~ADCSRA&(1<<ADIF)){}		// Result now available.
-		pot2 = ADCH;
-		ADCSRA |= (1<<ADIF);		// clear ADIF 
-		ADMUX &= ~((1<<2)|(1<<0));	// clear admux channels
+void sen_2() { // sensor 2 -> adc5
+	ADMUX = 0b11100101;
+	ADCSRB = 0b00000000;
+	ADCSRA |= (1<<6);
+	while(ADCSRA&(1<<ADSC)){}
+	pot2 = ADCH;
 }
 
-//void sen_1() { 
-//	ADMUX |= (1<<2);				// enable adc4 -> 000100
-//	while(~ADCSRA&(1<<ADIF)){}		// Result now available.
-//		pot1 = ADCH;
-//		ADCSRA |= (1<<ADIF);		// clear ADIF 
-//		ADMUX &= ~(1<<2);			// clear admux channels
-//}
+void sen_1() {  // sensor 1 -> adc4
+	ADMUX = 0b11100100;
+	ADCSRB = 0b00000000;
+	ADCSRA |= (1<<6);
+	while(ADCSRA&(1<<ADSC)){}
+	pot1 = ADCH;
+}
+
+void read_sensors(){
+//	sen_8();
+	sen_7();
+	sen_6();
+	sen_5();
+	sen_4();
+	sen_3();
+	sen_2();
+//	sen_1();
+}
 
 void setMotorSpeeds(double motorA, double motorB) {
 	OCR0A = 255 * motorA/100; 
@@ -140,10 +145,10 @@ int current_position(){
 
 	if(pot4 < TOLERANCE && pot5 < TOLERANCE){position = 0;} // Straight line case
 	if(
-	pot2 < TOLERANCE && pot3 < TOLERANCE &&
-	pot4 < TOLERANCE && pot5 < TOLERANCE &&
-	pot6 < TOLERANCE && pot7 < TOLERANCE
-	){position = 0;} // Straight line case
+		pot2 < TOLERANCE && pot3 < TOLERANCE &&
+		pot4 < TOLERANCE && pot5 < TOLERANCE &&
+		pot6 < TOLERANCE && pot7 < TOLERANCE
+		){position = 0;} // Straight line case
 
 	// if 2 sees line error =-5
 	if(pot2 < TOLERANCE && pot3 >= TOLERANCE && pot4 >= TOLERANCE && pot5 >= TOLERANCE && pot6 >= TOLERANCE && pot7 >= TOLERANCE){position =-5;}
@@ -166,20 +171,31 @@ int current_position(){
 	// if 7 sees line error = 5
 	if(pot7 < TOLERANCE && pot2 >= TOLERANCE && pot3 >= TOLERANCE && pot4 >= TOLERANCE && pot5 >= TOLERANCE && pot6 >= TOLERANCE){position = 5;}
 
-	setMotorSpeeds(Base - position , Base + position);
 	return position;
 }
 
 void pd(double kp, double kd, int base){
 	int current_pos = current_position();
 
-	if(
-		pot7 > TOLERANCE
-		&& pot6 > TOLERANCE 
-		&& pot5 > TOLERANCE
-		&& pot4 > TOLERANCE 
-		&& pot3 > TOLERANCE
-		&& pot2 > TOLERANCE 
+	if( safety = 1 &&
+		pot7 > tolerance
+		&& pot6 > tolerance 
+		&& pot5 > tolerance
+		&& pot4 > tolerance 
+		&& pot3 > tolerance
+		&& pot2 > tolerance 
+		){
+   			setmotorspeeds(0, 0);
+			delay(1000);
+		}
+
+	else if( safety = 0 &&
+		pot7 > tolerance
+		&& pot6 > tolerance 
+		&& pot5 > tolerance
+		&& pot4 > tolerance 
+		&& pot3 > tolerance
+		&& pot2 > tolerance 
 		){
 		error = *last_error;
 		}
@@ -196,31 +212,17 @@ int main(){
 	adc_init();
 	pwm_init();
 	while(1) {
-		ADCSRA |= (1<<6);	// start conversion
-
-		sen_7(); 
-		sen_6(); 
-		sen_5(); 
-		sen_4(); 
-		sen_3(); 
-		sen_2(); 
-
-		ADCSRA &= ~(1<<6);		// stop conversion
+   
+		read_sensors();
 		current_position();
 
-	if(position == -1 || position == 0 || position == 1){
-		Base = 70;
-		Kp = 15;
-	}
-	else if (position == -3 || position == -2 || position == 2 || position == 3){
-		Base = 60;
-		Kp = 19;
-	}
-	else{
-		Base = 35;
-		Kp = 6.8;
+		if(position == -1 | position == 0 | position == 1){
+			Base = 70;
+		}
+		else{
+			Base = 40;
+		}
 
-	}
 	pd(Kp, Kd, Base);
 	}
 }
--- src/sensor_test/sensor_test.ino
+++ src/sensor_test/sensor_test.ino
@@ -4,7 +4,7 @@
 #include <avr/interrupt.h>
 #include <stdlib.h>
 
-#define TOLERANCE 140
+#define TOLERANCE 110
 
 
 uint16_t pot8;
@@ -17,86 +17,76 @@ uint16_t pot2;
 uint16_t pot1;
 
 
-
-ISR(ADC_vect0)
-{
-	// Only read the sensor values.
-}
-
 void adc_init(){
-	ADMUX |= (1<<6)|(1<<5); //5V ref 
-	ADCSRA |= (1<<7)|(1<<5)|(1<<4)|(1<<3)|(1<<2)|(1<<1);
-	// enable adc, enable auto-trigger, enable interrupt & interrupt flag, clock prescaler of 64, start converion, left adjusted
+	ADMUX |= (1<<REFS1)|(1<<REFS0)|(1<<ADLAR); //2.56V ref, left adjusted
+
+	// enable adc, clock prescaler of 128
+	ADCSRA |= (1<<ADEN)|(1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0);
+	ADCSRB = 0;
 }
 
 void sen_8() {
-	ADCSRB |= (1<<5);	// enable adc 8 -> 100000
-	while(~ADCSRA&(1<<ADIF)){}	// Result now available.
-		pot8 = ADCH;
-		ADCSRA |= (1<<ADIF);	//clear adif
-		ADCSRB &= ~(1<<5);		// clear admux channels
+	ADMUX = 0b11100000;
+	ADCSRB = 0b00100000;		// enable adc 8 -> 100000
+	ADCSRA |= (1<<6);			// start conversion
+	while(ADCSRA&(1<<ADSC)){} 	// wait for conversion to complete
+	pot8 = ADCH;
 }
 
 void sen_7() {
-	ADMUX |= (1<<0);	// enable adc 9 -> 100001
-	ADCSRB |= (1<<5);
-	while(~ADCSRA&(1<<ADIF)){}	// Result now available.
-		pot7 = ADCH;
-		ADCSRA |= (1<<ADIF);	//clear adif
-		ADMUX &= ~(1<<0);		// clear admux channels
-		ADCSRB &= ~(1<<5);		// clear admux channels
+	ADMUX = 0b11100001; // sensor 7 -> adc9
+	ADCSRB = 0b00100000;
+	ADCSRA |= (1<<6);
+	while(ADCSRA&(1<<ADSC)){}
+	pot7 = ADCH;
 }
 
 void sen_6() {
-	ADMUX |= (1<<1);	//enable adc10 -> 100010
-	ADCSRB |= (1<<5);	// mux 5 on
-	while(~ADCSRA&(1<<ADIF)){}	// Result now available.
-		pot6 = ADCH;
-		ADCSRA |= (1<<ADIF);	//clear adif
-		ADMUX &= ~(1<<1);		// clear admux channels
-		ADCSRB &= ~(1<<5);		// clear admux channels
+	ADMUX = 0b11100010; // sensor 6 -> adc10
+	ADCSRB = 0b00100000;
+	ADCSRA |= (1<<6);
+	while(ADCSRA&(1<<ADSC)){}
+	pot6 = ADCH;
 }
 
 void sen_5() {
-	ADMUX |= (1<<1)|(1<<0);	//enable adc11 -> 100011
-	ADCSRB |= (1<<5);		// mux 5 on
-	while(~ADCSRA&(1<<ADIF)){}		// Result now available.
-		pot5 = ADCH;
-		ADCSRA |= (1<<ADIF);		//clear adif
-		ADMUX &= ~((1<<1)|(1<<0));	// clear admux channels
-		ADCSRB &= ~(1<<5);			// clear admux channels
+	ADMUX = 0b11100011; // sensor 5 -> adc11
+	ADCSRB = 0b00100000;
+	ADCSRA |= (1<<6);
+	while(ADCSRA&(1<<ADSC)){}
+	pot5 = ADCH;
 }
 
 void sen_4() { 
-	ADMUX |= (1<<2)|(1<<1)|(1<<0);			// enable adc7 -> 000111
-	while(~ADCSRA&(1<<ADIF)){}				// Result now available.
-		pot4 = ADCH;
-		ADCSRA |= (1<<ADIF);				// clear ADIF 
-		ADMUX &= ~((1<<2)|(1<<1)|(1<<0));	// clear admux channels
+	ADMUX = 0b11100111; // sensor 4 -> adc7
+	ADCSRB = 0b00000000;
+	ADCSRA |= (1<<6);
+	while(ADCSRA&(1<<ADSC)){}
+	pot4 = ADCH;
 }
 
-void sen_3() { 
-	ADMUX |= (1<<2)|(1<<1);			// enable adc6 -> 000110
-	while(~ADCSRA&(1<<ADIF)){}		// Result now available.
-		pot3 = ADCH;
-		ADCSRA |= (1<<ADIF);		// clear ADIF 
-		ADMUX &= ~((1<<2)|(1<<1));	// clear admux channels
+void sen_3() {  // sensor 3 -> adc6
+	ADMUX = 0b11100110;
+	ADCSRB = 0b00000000;
+	ADCSRA |= (1<<6);
+	while(ADCSRA&(1<<ADSC)){}
+	pot3 = ADCH;
 }
 
-void sen_2() { 
-	ADMUX |= (1<<2)|(1<<0);			// enable adc5 -> 000101
-	while(~ADCSRA&(1<<ADIF)){}		// Result now available.
-		pot2 = ADCH;
-		ADCSRA |= (1<<ADIF);		// clear ADIF 
-		ADMUX &= ~((1<<2)|(1<<0));	// clear admux channels
+void sen_2() { // sensor 2 -> adc5
+	ADMUX = 0b11100101;
+	ADCSRB = 0b00000000;
+	ADCSRA |= (1<<6);
+	while(ADCSRA&(1<<ADSC)){}
+	pot2 = ADCH;
 }
 
-void sen_1() { 
-	ADMUX |= (1<<2);				// enable adc4 -> 000100
-	while(~ADCSRA&(1<<ADIF)){}		// Result now available.
-		pot1 = ADCH;
-		ADCSRA |= (1<<ADIF);		// clear ADIF 
-		ADMUX &= ~(1<<2);			// clear admux channels
+void sen_1() {  // sensor 1 -> adc4
+	ADMUX = 0b11100100;
+	ADCSRB = 0b00000000;
+	ADCSRA |= (1<<6);
+	while(ADCSRA&(1<<ADSC)){}
+	pot1 = ADCH;
 }
 
 
@@ -104,8 +94,12 @@ void sen_1() {
 int main(){
 
 	adc_init();
+
+	DDRE |= (1<<6);
+	DDRB |= (1<<7)|(1<<6)|(1<<5)|(1<<2)|(1<<1)|(1<<0);
+	DDRD |= (1<<0);
+
 	while(1) {
-		ADCSRA |= (1<<6);	// start conversion
 
 		sen_8(); 
 		sen_7(); 
@@ -116,16 +110,27 @@ int main(){
 		sen_2(); 
 		sen_1(); 
 
-		ADCSRA &= ~(1<<6);		// stop conversion
-		if(	pot1 < TOLERANCE || pot2 < TOLERANCE ||
-			pot3 < TOLERANCE || pot4 < TOLERANCE ||
-			pot5 < TOLERANCE || pot6 < TOLERANCE ||
-			pot7 < TOLERANCE || pot8 < TOLERANCE
-		)
-		{
-		DDRE |= (1<<6);
-		PORTE |= (1<<6); 
+
+		if(pot1 < TOLERANCE)
+			{PORTE |= (1<<6);}
+ 		if(pot2 < TOLERANCE)
+			{PORTB |= (1<<0);}
+		else if(pot3 < TOLERANCE)
+			{PORTB |= (1<<1);}
+		else if(pot4 < TOLERANCE)
+			{PORTB |= (1<<2);}
+		else if(pot5 < TOLERANCE)
+			{PORTB |= (1<<7);}
+		else if(pot6 < TOLERANCE)
+			{PORTD |= (1<<0);}
+		else if(pot7 < TOLERANCE)
+			{PORTB |= (1<<6);}
+		else if(pot8 < TOLERANCE)
+			{PORTB |= (1<<5);}
+		else{
+			PORTE &= ~(1<<6);
+			PORTB &= ~((1<<7)|(1<<6)|(1<<5)|(1<<2)|(1<<1)|(1<<0));
+			PORTD &= ~(1<<0);
 		}
-		else{PORTE &= ~(1<<6);}
 	}
 }
